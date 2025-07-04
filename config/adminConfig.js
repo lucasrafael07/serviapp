cef410bb4a9429319a0a901af31ab9b09b68d36f
Lucas Rafael <lucasrafael07@users.noreply.github.com>
2025-07-04 17:27:39 -0300

    admin

---
+++ config/adminConfig.js
@@ -0,0 +1,8 @@
+// config/adminConfig.js
+
+// Adicione aqui o email dos administradores.
+// Você pode adicionar mais de um, separando por vírgula.
+export const ADMIN_EMAILS = [
+  "admin@serviapp.com",
+  "seu.email.de.admin@exemplo.com" 
+];

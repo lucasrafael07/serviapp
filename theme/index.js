b5cb7caaa6306a9f423cfdd4d9323a135b246bd0
Lucas Rafael <lucasrafael07@users.noreply.github.com>
2025-07-04 17:00:39 -0300

    Create index.js

---
+++ theme/index.js
@@ -0,0 +1,17 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
+const colors = {
+  brand: {
+    50: '#fffde7', 100: '#fff9c4', 200: '#fff59d',
+    300: '#fff176', 400: '#ffee58', 500: '#ffeb3b',
+    600: '#fdd835', 700: '#fbc02d', 800: '#f9a825',
+    900: '#f57f17',
+  },
+};
+
+export const theme = extendTheme({ config, colors });

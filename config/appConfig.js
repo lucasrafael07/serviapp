813f994473977f1a213eabb6d12a2acf8188a7c3
Lucas Rafael <lucasrafael07@users.noreply.github.com>
2025-07-04 17:14:38 -0300

    Create appConfig.js

---
+++ config/appConfig.js
@@ -0,0 +1,5 @@
+export const CATEGORIAS_PRINCIPAIS = [
+  "Casa e Construção", "Tecnologia e Informática", "Design e Criatividade",
+  "Aulas e Educação", "Saúde e Bem-estar", "Serviços Automotivos",
+  "Eventos e Festas", "Consultoria e Negócios", "Cuidados Pessoais"
+];
